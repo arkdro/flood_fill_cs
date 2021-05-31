703f88d4559697a180860e455bf3c4410a2c560e
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 21:58:18 +0200

    add a test

--- tests/Fill.Test/UnitTest1.cs
+++ tests/Fill.Test/UnitTest1.cs
@@ -92,6 +92,38 @@ namespace Fill.Test
         }
 
         [Test]
+        public void FillTest04_fill_region()
+        {
+            var f = new Fill();
+            var data = new int[,] {
+                {11, 10, 11, 11, 11, 11, 11, 10},
+                {11, 10, 11, 13, 13, 10, 11, 11},
+                {11, 10, 11, 13, 11, 11, 10, 11},
+                {11, 10, 11, 13, 13, 11, 10, 11},
+                {11, 10, 11, 11, 11, 11, 10, 11},
+                {11, 11, 17, 12, 12, 17, 17, 11},
+                {12, 11, 11, 11, 11, 11, 11, 11}
+            };
+            var new_data = f.fill(input: data, start_x: 2, start_y: 0, target_color: 11, replacement_color: 21);
+            var expected = new int[,] {
+                {21, 10, 21, 21, 21, 21, 21, 10},
+                {21, 10, 21, 13, 13, 10, 21, 21},
+                {21, 10, 21, 13, 21, 21, 10, 21},
+                {21, 10, 21, 13, 13, 21, 10, 21},
+                {21, 10, 21, 21, 21, 21, 10, 21},
+                {21, 21, 17, 12, 12, 17, 17, 21},
+                {12, 21, 21, 21, 21, 21, 21, 21}
+            };
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Assert.AreEqual(expected[y, x], new_data[y, x], $"expected: {expected[y, x]}, actual: {new_data[y, x]}, x: {x}, y: {y}");
+                }
+            }
+        }
+
+        [Test]
         public void extract_result_test()
         {
             var f = new Fill();
