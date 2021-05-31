435ad748a85ee6d24166c22058afdbb1ad916d45
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 21:58:17 +0200

    add a test

--- tests/Fill.Test/UnitTest1.cs
+++ tests/Fill.Test/UnitTest1.cs
@@ -1,5 +1,9 @@
 using NUnit.Framework;
 
+using System;
+
+using Fill;
+
 namespace Fill.Test
 {
     public class Tests
@@ -16,9 +20,19 @@ namespace Fill.Test
         }
 
         [Test]
-        public void Test2()
+        public void FillTest01()
         {
-            Assert.Fail();
+            var f = new Fill();
+            var data = new int[,] {{11,12}, {11,14}, {11,11}, {17,11}};
+            var new_data = f.fill(data, 0, 1, 14, 21);
+            var expected = new int[,] {{11,12}, {11,21}, {11,11}, {17,11}};
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Assert.AreEqual(expected[y, x], new_data[y, x]);
+                }
+            }
         }
     }
 }
