0b9886291ad2bd8e199d2ab3c68d793c4ae4b212
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 21:58:17 +0200

    add a test

--- tests/Fill.Test/UnitTest1.cs
+++ tests/Fill.Test/UnitTest1.cs
@@ -36,6 +36,34 @@ namespace Fill.Test
         }
 
         [Test]
+        public void FillTest02_fill_small_region()
+        {
+            var f = new Fill();
+            var data = new int[,] {
+                {11, 10, 11, 11},
+                {11, 10, 13, 11},
+                {11, 11, 11, 11},
+                {12, 17, 17, 14},
+                {12, 12, 11, 11}
+            };
+            var new_data = f.fill(input: data, start_x: 2, start_y: 4, target_color: 11, replacement_color: 21);
+            var expected = new int[,] {
+                {11, 10, 11, 11},
+                {11, 10, 13, 11},
+                {11, 11, 11, 11},
+                {12, 17, 17, 14},
+                {12, 12, 21, 21}
+            };
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Assert.AreEqual(expected[y, x], new_data[y, x], $"expected: {expected[y, x]}, actual: {new_data[y, x]}, x: {x}, y: {y}");
+                }
+            }
+        }
+
+        [Test]
         public void extract_result_test()
         {
             var f = new Fill();
