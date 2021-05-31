4b6f5eee5d81967db4d0444f83edc1ca9c8e94be
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 21:58:17 +0200

    extract_result_test

--- tests/Fill.Test/UnitTest1.cs
+++ tests/Fill.Test/UnitTest1.cs
@@ -34,5 +34,26 @@ namespace Fill.Test
                 }
             }
         }
+
+        [Test]
+        public void extract_result_test()
+        {
+            var f = new Fill();
+            var data = new Point[,] {
+                {new Point(state:State.New, color: 11), new Point(state:State.New, color: 12)},
+                {new Point(state:State.New, color: 11), new Point(state:State.New, color: 14)},
+                {new Point(state:State.New, color: 11), new Point(state:State.New, color: 11)},
+                {new Point(state:State.New, color: 17), new Point(state:State.New, color: 11)},
+            };
+            var extracted = f.extract_result(data);
+            var expected = new int[,] {{11,12}, {11,14}, {11,11}, {17,11}};
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Assert.AreEqual(expected[y, x], extracted[y, x]);
+                }
+            }
+        }
     }
 }
